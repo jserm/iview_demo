6fb65b458e201975968a0615c5367d0f5265645f
Jenson <56425444+jserm@users.noreply.github.com>
2020-10-28 00:32:55 +0800

    iview研究

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,98 +1,200 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <div>
+      <h5>this is a iview demo</h5>
+      <Form
+        ref="formValidate"
+        :model="formValidate"
+        :rules="ruleValidate"
+        :label-width="80"
+      >
+        <FormItem label="Name" prop="name">
+          <Input
+            v-model="formValidate.name"
+            placeholder="Enter your name"
+          ></Input>
+        </FormItem>
+        <FormItem label="E-mail" prop="mail">
+          <Input
+            v-model="formValidate.mail"
+            placeholder="Enter your e-mail"
+          ></Input>
+        </FormItem>
+        <FormItem label="City" prop="city">
+          <Select v-model="formValidate.city" placeholder="Select your city">
+            <Option value="beijing">New York</Option>
+            <Option value="shanghai">London</Option>
+            <Option value="shenzhen">Sydney</Option>
+          </Select>
+        </FormItem>
+        <FormItem label="Date">
+          <Row>
+            <Col span="11">
+              <FormItem prop="date">
+                <DatePicker
+                  type="date"
+                  placeholder="Select date"
+                  v-model="formValidate.date"
+                ></DatePicker>
+              </FormItem>
+            </Col>
+            <Col span="2" style="text-align: center">-</Col>
+            <Col span="11">
+              <FormItem prop="time">
+                <TimePicker
+                  type="time"
+                  placeholder="Select time"
+                  v-model="formValidate.time"
+                ></TimePicker>
+              </FormItem>
+            </Col>
+          </Row>
+        </FormItem>
+        <FormItem label="Gender" prop="gender">
+          <RadioGroup v-model="formValidate.gender">
+            <Radio label="male">Male</Radio>
+            <Radio label="female">Female</Radio>
+          </RadioGroup>
+        </FormItem>
+        <FormItem label="Hobby" prop="interest">
+          <CheckboxGroup v-model="formValidate.interest">
+            <Checkbox label="Eat"></Checkbox>
+            <Checkbox label="Sleep"></Checkbox>
+            <Checkbox label="Run"></Checkbox>
+            <Checkbox label="Movie"></Checkbox>
+          </CheckboxGroup>
+        </FormItem>
+        <FormItem label="Desc" prop="desc">
+          <Input
+            v-model="formValidate.desc"
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 5 }"
+            placeholder="Enter something..."
+          ></Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary" @click="handleSubmit('formValidate')"
+            >Submit</Button
+          >
+          <Button @click="handleReset('formValidate')" style="margin-left: 8px"
+            >Reset</Button
+          >
+        </FormItem>
+      </Form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String
-  }
-};
+    msg: String,
+  },
+  data() {
+    return {
+      formValidate: {
+        name: '',
+        mail: '',
+        city: '',
+        gender: '',
+        interest: [],
+        date: '',
+        time: '',
+        desc: '',
+      },
+      ruleValidate: {
+        name: [
+          {
+            required: true,
+            message: 'The name cannot be empty',
+            trigger: 'blur',
+          },
+        ],
+        mail: [
+          {
+            required: true,
+            message: 'Mailbox cannot be empty',
+            trigger: 'blur',
+          },
+          { type: 'email', message: 'Incorrect email format', trigger: 'blur' },
+        ],
+        city: [
+          {
+            required: true,
+            message: 'Please select the city',
+            trigger: 'change',
+          },
+        ],
+        gender: [
+          {
+            required: true,
+            message: 'Please select gender',
+            trigger: 'change',
+          },
+        ],
+        interest: [
+          {
+            required: true,
+            type: 'array',
+            min: 1,
+            message: 'Choose at least one hobby',
+            trigger: 'change',
+          },
+          {
+            type: 'array',
+            max: 2,
+            message: 'Choose two hobbies at best',
+            trigger: 'change',
+          },
+        ],
+        date: [
+          {
+            required: true,
+            type: 'date',
+            message: 'Please select the date',
+            trigger: 'change',
+          },
+        ],
+        time: [
+          {
+            required: true,
+            type: 'string',
+            message: 'Please select time',
+            trigger: 'change',
+          },
+        ],
+        desc: [
+          {
+            required: true,
+            message: 'Please enter a personal introduction',
+            trigger: 'blur',
+          },
+          {
+            type: 'string',
+            min: 20,
+            message: 'Introduce no less than 20 words',
+            trigger: 'blur',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    handleSubmit(name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          this.$Message.success('Success!')
+        } else {
+          this.$Message.error('Fail!')
+        }
+      })
+    },
+    handleReset(name) {
+      this.$refs[name].resetFields()
+    },
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -100,13 +202,10 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.hello {
+  display: flex;
+  justify-content: center;
 }
 a {
   color: #42b983;
